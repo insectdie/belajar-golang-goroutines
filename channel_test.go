c7f811c2c9e7946829d97d6271b2c248469a7e46
insectdie <ompusungguandry@gmail.com>
2022-02-21 16:24:59 +0700

    Channel In Out

--- channel_test.go
+++ channel_test.go
@@ -34,3 +34,23 @@ func TestChannelAsParameter(t *testing.T) {
 	fmt.Println(data)
 	close(channel)
 }
+
+func OnlyIn(channel chan<- string) {
+	time.Sleep(2 * time.Second)
+	channel <- "Andry Halomoan Ompusunggu"
+}
+
+func OnlyOut(channel <-chan string) {
+	data := <-channel
+	fmt.Println(data)
+}
+
+func TestInOutChannel(t *testing.T) {
+	channel := make(chan string)
+	defer close(channel)
+
+	go OnlyIn(channel)
+	go OnlyOut(channel)
+
+	time.Sleep(5 * time.Second)
+}
