e82c7eac9ced5f03a7f9e063ae675f532778b1eb
insectdie <ompusungguandry@gmail.com>
2022-02-21 16:42:24 +0700

    Range Channel

--- channel_test.go
+++ channel_test.go
@@ -2,6 +2,7 @@ package belajargolanggoroutines
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 	"time"
 )
@@ -60,6 +61,23 @@ func TestBufferedChannel(t *testing.T) {
 	defer close(channel)
 
 	channel <- "Andry"
+
+	fmt.Println("Selesai")
+}
+
+func TestRangeChannel(t *testing.T) {
+	channel := make(chan string)
+
+	go func() {
+		for i := 0; i < 10; i++ {
+			channel <- "Perulangan Ke - " + strconv.Itoa(i)
+		}
+		close(channel)
+	}()
+
+	for data := range channel {
+		fmt.Println("Menerima Data", data)
+	}
 
 	fmt.Println("Selesai")
 }
