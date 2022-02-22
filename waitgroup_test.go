52db15525a32c697cb374e20c3fa0ae80cabd307
insectdie <ompusungguandry@gmail.com>
2022-02-22 21:54:23 +0700

    WaitGroup

---
+++ waitgroup_test.go
@@ -0,0 +1,28 @@
+package belajargolanggoroutines
+
+import (
+	"fmt"
+	"sync"
+	"testing"
+	"time"
+)
+
+func RunAsychronus(group *sync.WaitGroup) {
+	defer group.Done()
+
+	group.Add(1)
+
+	fmt.Println("Hello")
+	time.Sleep(1 * time.Second)
+}
+
+func TestWaitGroup(t *testing.T) {
+	group := &sync.WaitGroup{}
+
+	for i := 0; i < 100; i++ {
+		go RunAsychronus(group)
+	}
+
+	group.Wait()
+	fmt.Println("Selesai")
+}
